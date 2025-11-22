4563272ca68912dbd513f7b6f27bc5f7773c242b
Daniel Zhang <xiaozhangshiw@163.com>
2025-11-22 18:31:20 +0800

    Update Chinese translations for button interactions and temporarily hide graph viewing feature

--- src/main/java/com/faridfaharaj/profitable/tasks/gui/guis/AssetExplorer.java
+++ src/main/java/com/faridfaharaj/profitable/tasks/gui/guis/AssetExplorer.java
@@ -135,9 +135,9 @@ public final class AssetExplorer extends ChestGUI {
                 if(click.isLeftClick()){
                     button.trade(player, assetCache);
                 }
-                if(click.isRightClick()){
-                    button.graphs(player, assetCache);
-                }
+//                if(click.isRightClick()){
+//                    button.graphs(player, assetCache);
+//                } 暂时隐藏该功能
             }
         }
 
